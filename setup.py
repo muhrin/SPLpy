8a36e83751cd2c036afc5bcbcb653dcf6afeae96
Martin Uhrin <martin.uhrin@gmail.com>
2014-07-07 15:30:15 +0100

    Minor change to setup.pu

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
@@ -7,8 +7,12 @@ setup(
     name='splpy',
     description='SPLpy',
     author='Martin Uhrin',
+    author_email='martin.uhrin.10@ucl.ac.uk',
     version='0.1.0',
     install_requires=['nose', 'pymatgen', 'pymatgen-db', 'pymongo'],
-    packages=['spl'],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Development Status:: 2 - Pre - Alpha"]
 )
 
